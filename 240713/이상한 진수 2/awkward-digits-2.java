f4ee326ad323bfbb7b8019ee8aefc73e105cf48c
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-07-13 22:42:51 +0900

    Title: 이상한 진수 2, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240713/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String a = sc.next();
+        String newStr="";
+        for(int i=0;i<a.length();i++){            
+            if(a.charAt(i) == '0'){
+                StringBuilder sb = new StringBuilder(a);
+                sb.setCharAt(i, '1');
+                newStr = sb.toString();
+                break;
+
+            }
+        }
+        int sum=0;
+        for(int i=0;i<newStr.length();i++){
+            if(newStr.charAt(i)=='1'){
+                int exp = newStr.length()-1-i;
+                sum += (int)Math.pow(2,exp);
+                
+            }
+        }
+        System.out.println(sum);
+        
+    }
+}
