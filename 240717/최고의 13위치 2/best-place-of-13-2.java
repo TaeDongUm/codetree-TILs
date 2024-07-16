130cf1a52de1b9640dc9eadafa5e1625ad032821
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-07-17 01:02:11 +0900

    Title: 최고의 13위치 2, Time: 159ms, Memory: 9MB, Status: Passed - CodeTree

--- 240717/최고의 13위치 2/best-place-of-13-2.java
+++ 240717/최고의 13위치 2/best-place-of-13-2.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int answer = Integer.MIN_VALUE;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -15,21 +16,38 @@ public class Main {
             }
         }
 
-        LinkedList<Integer> maxCoins = new LinkedList<>();
+        LinkedList<LinkedList<Integer>> maxCoins = new LinkedList<>();
 
         for(int i=0;i<N;i++){
             int coins=0;
             for(int j=0;j<N;j++){
                 if(j+2 < N){
                     coins += coinsMap[i][j] + coinsMap[i][j+1] + coinsMap[i][j+2];
-                    maxCoins.add(coins);
-                    coins=0;
-                    j = j+2;
+                    for(int k=0;k<N;k++){
+                        for(int l=0;l<N;l++){
+                            if(i==k){
+                                if(j+2 <l && l+2 <N){
+                                    coins += coinsMap[k][l] + coinsMap[k][l+1] + coinsMap[k][l+2];                                    
+                                    answer = Math.max(answer, coins);
+                                    coins -= coinsMap[k][l] + coinsMap[k][l+1] + coinsMap[k][l+2];
+
+                                }
+                            }else{
+                                if(l+2 < N){
+                                    coins += coinsMap[k][l] + coinsMap[k][l+1] + coinsMap[k][l+2];
+                                    answer = Math.max(answer, coins);
+                                    coins -= coinsMap[k][l] + coinsMap[k][l+1] + coinsMap[k][l+2];
+                                }
+                            }
+
+                            
+                        }
+                    }
                 }
+                coins=0;
+
             }
         }
-        Collections.sort(maxCoins, Collections.reverseOrder());
-        int answer = maxCoins.get(0) + maxCoins.get(1);
         System.out.println(answer);
     }
 }
