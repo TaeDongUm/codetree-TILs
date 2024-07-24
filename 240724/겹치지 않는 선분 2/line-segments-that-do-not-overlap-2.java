46dc1bf92cebd1f6aa5b04693a61936432de6ed7
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-07-24 23:11:07 +0900

    Title: 겹치지 않는 선분 2, Time: 108ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240724/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        LinkedList<float[]> list = new LinkedList<>();
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            float[] value = new float[3];
+            float x1 = Float.parseFloat(st.nextToken());
+            float x2 = Float.parseFloat(st.nextToken());
+            float incli = (-1)/(x1-x2);            
+            value[0] = x1;
+            value[1] = x2;
+            value[2] = incli;
+            list.add(value);
+
+        }
+        int answer=0;
+
+        for(int i=0;i<list.size();i++){
+            float start1 = list.get(i)[0];
+            float end1 = list.get(i)[1];
+            float inc1 = list.get(i)[2];
+            // (inc2-inc1)*x = (inc2*start2+inc1*start1)
+            for(int j=i+1;j<list.size();j++){
+                float start2 = list.get(j)[0];
+                float end2 = list.get(j)[1];
+                float inc2 = list.get(j)[2];
+                float compare = (inc2*start2 + inc1*start1)/(inc2-inc1);
+                if(start1 <=compare && compare <= end1 ) answer++;
+            }
+        }
+        answer++;
+        System.out.println(answer);
+        
+    }
+}
