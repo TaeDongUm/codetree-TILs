8cda9f1faca93f7a5340568663bfe19f569a96ab
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-07-25 00:13:40 +0900

    Title: 겹치지 않는 선분 2, Time: 109ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240725/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
+++ 240725/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -2,42 +2,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        LinkedList<double[]> list = new LinkedList<>();
-        for(int i=0;i<N;i++){
-            st = new StringTokenizer(br.readLine());
-            double[] value = new double[3];
-            double x1 = Double.parseDouble(st.nextToken());
-            double x2 = Double.parseDouble(st.nextToken());
-            double incli = (double)(-1)/(x1-x2);            
-            value[0] = x1;
-            value[1] = x2;
-            value[2] = incli;
-            list.add(value);
+        int[][] segments = new int[N][2];
 
+        // 선분 입력 받기
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            segments[i][0] = x1;
+            segments[i][1] = x2;
         }
-        int answer=N;        
 
-        for(int i=0;i<list.size();i++){
-            double start1 = list.get(i)[0];
-            double end1 = list.get(i)[1];
-            double inc1 = list.get(i)[2];
-            // (inc2-inc1)*x = (inc2*start2+inc1*start1)
-            boolean isTrue = true;
-            for(int j=0;j<list.size();j++){
-                if(i==j) continue;
-                double start2 = list.get(j)[0];
-                double end2 = list.get(j)[1];
-                double inc2 = list.get(j)[2];
-                double compare = (inc2*start2 - inc1*start1)/(inc2-inc1);
-                if(start1 <=compare && compare <= end1 ) isTrue = false;
+        int count = 0;
+
+        // 선분 간의 교차 여부를 판단
+        for (int i = 0; i < N; i++) {
+            boolean isSpecial = true;
+            int x1 = segments[i][0];
+            int x2 = segments[i][1];
+
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+
+                int y1 = segments[j][0];
+                int y2 = segments[j][1];
+
+                // 선분 A: (x1, 0) to (x2, 1)
+                // 선분 B: (y1, 0) to (y2, 1)
+
+                // 선분 A와 선분 B의 기울기
+                double inc1 = (1.0) / (x2 - x1);
+                double inc2 = (1.0) / (y2 - y1);
+
+                // 교차점 계산
+                double compare = (inc2 * y1 - inc1 * x1) / (inc2 - inc1);
+
+                // 교차점이 선분 A의 범위 내에 있는지 확인
+                if (x1 <= compare && compare <= x2) {
+                    isSpecial = false;
+                    break;
+                }
             }
-            if(!isTrue) answer--;
-        }        
-        System.out.println(answer);
-        
+
+            if (isSpecial) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
