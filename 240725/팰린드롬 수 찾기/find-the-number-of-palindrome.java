95ba52a962e3bffba77ea225d67eb524d0428d71
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-07-25 21:41:40 +0900

    Title: 팰린드롬 수 찾기, Time: 114ms, Memory: 8MB, Status: Passed - CodeTree

--- 240725/팰린드롬 수 찾기/find-the-number-of-palindrome.java
+++ 240725/팰린드롬 수 찾기/find-the-number-of-palindrome.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
@@ -10,29 +10,26 @@ public class Main {
         int X = Integer.parseInt(st.nextToken());
         int Y = Integer.parseInt(st.nextToken());
 
-        int answer=0;
+        int answer = 0;
 
-        for(int i=X;i<=Y;i++){
-            int count =0;
-            int compareNum = i;
-            while(Math.pow(10,count) <= compareNum){
-                count++;
-            }
-            count--;
-            int palindrome = 0;
-
-            while(compareNum>0){
-                int tmp = compareNum%10;
-                palindrome += Math.pow(10, count)*tmp;
-                compareNum = compareNum/10;
-                count--;
-            }
-            if(i == palindrome){
+        for (int i = X; i <= Y; i++) {
+            if (isPalindrome(i)) {
                 answer++;
             }
-            
         }
         System.out.println(answer);
+    }
 
+    public static boolean isPalindrome(int number) {
+        int original = number;
+        int reversed = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        return original == reversed;
     }
 }
