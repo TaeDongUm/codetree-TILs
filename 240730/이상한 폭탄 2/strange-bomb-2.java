7fb275abacb9a902d5eab40255c344362646c912
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-07-30 23:59:48 +0900

    Title: 이상한 폭탄 2, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240730/이상한 폭탄 2/strange-bomb-2.java
+++ 240730/이상한 폭탄 2/strange-bomb-2.java
@@ -4,26 +4,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
 
-        for (int i = 0; i < N; i++) {
+        for(int i=0;i<N;i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
-
-        int answer = -1;  // 초기 값을 -1로 설정
-
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j <= i + K && j < N; j++) {
-                if (arr[i] == arr[j]) {
-                    answer = Math.max(answer, arr[i]);
-                }
+        int answer = Integer.MIN_VALUE;
+        for(int i=0;i<N;i++){
+            for(int j=i+1;j<i+1+K && j<N;j++){
+                if(arr[i] == arr[j]){
+                    if(answer < arr[i]){
+                        answer = arr[i];
+                    }        
+                } 
             }
         }
-
         System.out.println(answer);
     }
 }
