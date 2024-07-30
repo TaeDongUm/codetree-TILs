011cad506b993dc78c8ba02fa4aa70265c632717
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-07-30 23:57:34 +0900

    Title: 이상한 폭탄 2, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240730/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int answer = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length-K;i++){
+            for(int j=i+1;j<i+1+K && j<N;j++){
+                if(arr[i] == arr[j]){
+                    if(answer < arr[i]){
+                        answer = arr[i];
+                    }        
+                } 
+            }
+        }
+        System.out.println(answer);
+    }
+}
