876493b24862427dd1c139fb19ab2014b8e5c35f
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-08-01 16:46:58 +0900

    Title: 상해버린 치즈, Time: 106ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240801/상해버린 치즈/rotten-cheese.java
+++ 240801/상해버린 치즈/rotten-cheese.java
@@ -14,6 +14,7 @@ public class Main {
         int S = Integer.parseInt(st.nextToken());
 
         LinkedList<int[]> list = new LinkedList<>();
+        int[][] checkPeople = new int[M][N];
         int[] visited = new int[N];
         int[] cheeze = new int[M];
 
@@ -31,50 +32,48 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int person = Integer.parseInt(st.nextToken())-1;
             int sick = Integer.parseInt(st.nextToken());
+
+            // 아프다고 한 사람 visited 배열로 체크하기
+            // S값으로 주어진 사람들만 상한 치즈 카운팅 하기
             visited[person] =1;
 
             int[] cheezeCheck = new int[M];
             for(int j=0;j<list.size();j++){
-                if(list.get(j)[0] == person && list.get(j)[2] < sick && cheezeCheck[list.get(j)[1]]==0){
+                if(list.get(j)[0] == person && list.get(j)[2] < sick && list.get(j)[1] < M && cheezeCheck[list.get(j)[1]]==0){
+                    checkPeople[list.get(j)[1]][person] =1;
                     cheeze[list.get(j)[1]] += 1;
                     cheezeCheck[list.get(j)[1]] = 1;
                 }
             }
-
         }
 
+        // S값으로 주어지지 않은 사람들 상한 치즈 카운팅 하기
         for(int i=0;i<visited.length;i++){
             int[] cheezeCheck = new int[M];
             if(visited[i] == 0){
                 for(int j=0;j<list.size();j++){
-                    if(list.get(j)[0] == i && cheezeCheck[list.get(j)[1]] ==0 ){
+                    if(list.get(j)[0] == i && cheezeCheck[list.get(j)[1]] ==0 && list.get(j)[1] < M ){
+                        checkPeople[list.get(j)[1]][i] =1;
                         cheeze[list.get(j)[1]] +=1;
                         cheezeCheck[list.get(j)[1]] =1;
                     }
                 }
             }
         }
+        int answer = Integer.MIN_VALUE;
 
-        int answer = 0;
-        // for(int i=0;i<cheeze.length;i++){
-        //     int[] checkPeople = new int[N];
-        //     boolean isTrue = true;
-        //     for(int j=0;j<list.size();j++){
-        //         if(i == list.get(j)[1] && checkPeople[list.get(j)[0]]==0){
-        //             checkPeople[list.get(j)[0]] =1;
-        //         }else if(i == list.get(j)[1] && checkPeople[list.get(j)[0]] !=0){
-        //             cheeze[list.get(j)[1]] -=1;
-        //             isTrue = false;
-        //             break;
-        //         } 
-        //     }
-        //     if(isTrue){
-        //         if(answer < cheeze[i]) answer = cheeze[i];
-        //     }
-            
-        // }        
+        // 각 치즈가 입력값인 아픈 사람들을 포함하는지 확인하기
         for(int i=0;i<cheeze.length;i++){
-            answer = Math.max(answer, cheeze[i]);
+            boolean isTrue = false;
+            for(int k=0;k<visited.length;k++){
+                for(int j=0;j<checkPeople[i].length;j++){
+                    if(visited[k] ==0) continue;
+                    if(visited[k] == checkPeople[i][j] ) isTrue = true;
+                }
+            }
+            if(isTrue){
+                if(answer < cheeze[i]) answer = cheeze[i];
+            }
         }
         System.out.println(answer);
         
