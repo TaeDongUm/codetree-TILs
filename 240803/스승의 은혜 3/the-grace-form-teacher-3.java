88bc16d04e2dbec5ff187c23b42cadf32a459903
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-08-03 04:13:14 +0900

    Title: 스승의 은혜 3, Time: 3066ms, Memory: 11MB, Status: Time Limit Exceed - CodeTree

--- 240803/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240803/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -2,57 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int answer = Integer.MIN_VALUE;
     static int N, B;
-    static int[][] students;
-    static boolean[] visited;
-    static int answer = 0;
-    
-    public static void main(String[] args) throws Exception {
+    static int countCheck=0;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
 
-        students = new int[N][2];
-        for (int i = 0; i < N; i++) {
+        // 1. 각 학생이 쿠폰을 받을 학생이라고 가정한다.
+        // 2. dfs를 통해서 각 학생을 뺄지 포함할지 선택한다.
+
+        LinkedList<int[]> list = new LinkedList<>();
+        int[] pickStudent = new int[N];
+        int money =0;
+        int student = N;
+
+        for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
-            students[i][0] = Integer.parseInt(st.nextToken()); // 가격
-            students[i][1] = Integer.parseInt(st.nextToken()); // 배송비
+            int[] arr = new int[2];
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            money += arr[0] + arr[1];
+            list.add(arr);
         }
 
-        // DFS 시작
-        dfs(0, B, 0);
+        // 한 학생을 쿠폰 적용 대상자로 선택
+        for(int i=0;i<list.size();i++){
+            int tmpMoney = money - list.get(i)[0]/2;
+            pickStudent[i] = 1;
+            dfs(list, student, tmpMoney, pickStudent);
+            pickStudent[i] = 0;
+            
+        }
         System.out.println(answer);
+
+    }
+    public static void dfs(LinkedList<int[]> list, int student, int money, int[] pickStudent){
+        if(B >= money){
+            if(student > answer){
+                answer = student;
+                return;
+            } 
+            
+        }
+        for(int i=0;i<list.size();i++){
+            if(pickStudent[i] == 1) continue;
+            pickStudent[i] =1;
+            int tmpMoney = money -(list.get(i)[0] + list.get(i)[1]);
+            dfs(list, student -1, tmpMoney, pickStudent); // 해당 학생을 뺸다
+            // dfs(list, student, money, pickStudent); // 해당 학생을 빼지 않는다.
+            pickStudent[i] =0;
+
+        }
     }
 
-    public static void dfs(int index, int remainingBudget, int count) {
-        // 가지치기: 예산 초과 시 조기에 종료
-        if (remainingBudget < 0) {
-            return;
-        }
-        
-        // 현재 최대 학생 수 업데이트
-        if (count > answer) {
-            answer = count;
-        }
-        
-        // 모든 학생을 방문하는 경우 종료
-        if (index >= N) {
-            return;
-        }
-        
-        // 현재 학생을 포함하지 않는 경우
-        dfs(index + 1, remainingBudget, count);
-        
-        // 현재 학생을 포함하는 경우
-        int price = students[index][0];
-        int shipping = students[index][1];
-        
-        // 쿠폰 사용 시
-        int discountedPrice = price / 2;
-        dfs(index + 1, remainingBudget - discountedPrice - shipping, count + 1);
-        
-        // 쿠폰 미사용 시
-        dfs(index + 1, remainingBudget - price - shipping, count + 1);
-    }
+
 }
