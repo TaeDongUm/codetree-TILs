a52ce4437e20fe5b82ce4e4bb4db542cdfefc018
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-08-03 05:29:09 +0900

    Title: 스승의 은혜 3, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240803/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240803/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int answer = Integer.MIN_VALUE;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -9,43 +10,48 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
+        LinkedList<int[]> list = new LinkedList<>();
+        int maxStudents=0;
         
-        int[][] students = new int[N][2];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            students[i][0] = Integer.parseInt(st.nextToken());
-            students[i][1] = Integer.parseInt(st.nextToken());
+            int[] students = new int[2];
+            students[0] = Integer.parseInt(st.nextToken());
+            students[1] = Integer.parseInt(st.nextToken());
+            list.add(students);
         }
-        
-        int maxStudents = 0;
-        
-        for (int i = 0; i < N; i++) {
-            // i번째 학생의 선물을 반값으로 구매
-            int discountCost = (students[i][0] / 2) + students[i][1];
-            
-            // i번째 학생을 포함한 비용 계산
-            int totalCost = discountCost;
-            List<Integer> otherCosts = new ArrayList<>();
-            for (int j = 0; j < N; j++) {
-                if (i != j) {
-                    otherCosts.add(students[j][0] + students[j][1]);
-                }
+        Collections.sort(list, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b){
+                int sumA = a[0] + a[1];
+                int sumB = b[0] + b[1];
+                return Integer.compare(sumA, sumB);
             }
-            Collections.sort(otherCosts);
-            
-            int count = 1; // i번째 학생을 포함하므로 1부터 시작
-            for (int cost : otherCosts) {
-                if (totalCost + cost <= B) {
-                    totalCost += cost;
-                    count++;
-                } else {
-                    break;
-                }
-            }
-            
-            maxStudents = Math.max(maxStudents, count);
+        });
+        int money =0;
+
+        for(int i=0;i<list.size();i++){
+            money += list.get(i)[0] + list.get(i)[1];
+            maxStudents++;
         }
-        
-        System.out.println(maxStudents);
+        for(int i=0;i<list.size();i++){
+            int tmpMoney = money - list.get(i)[0]/2; 
+            int students = remove(list, i, tmpMoney, maxStudents, B);
+            if(answer < students) answer = students;
+        }
+        System.out.println(answer);
+ 
+    }
+    public static int remove(LinkedList<int[]> list, int idx, int tmpMoney, int Students, int B){
+        for(int i=list.size()-1;i>0;i--){
+            if(i == idx) continue;
+
+            tmpMoney -= list.get(i)[0] + list.get(i)[1];
+            Students--;
+            if(B >= tmpMoney){
+                return Students;
+            }
+        }
+        return Students;
     }
 }
