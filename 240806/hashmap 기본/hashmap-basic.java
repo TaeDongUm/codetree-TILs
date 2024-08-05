0626f2c0de76f05da5455aa6f55d6af136874934
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-08-06 00:52:56 +0900

    Title: hashmap 기본, Time: 772ms, Memory: 21MB, Status: Passed - CodeTree

--- 240806/hashmap 기본/hashmap-basic.java
+++ 240806/hashmap 기본/hashmap-basic.java
@@ -16,15 +16,12 @@ public class Main {
             if(input.equals("add")){
                 key = Integer.parseInt(st.nextToken());
                 value = Integer.parseInt(st.nextToken());
-            }else{
-                key =Integer.parseInt(st.nextToken());
-            }
-
-            if(input.equals("add")){
                 map.put(key, value);
             }else if(input.equals("remove")){
+                key =Integer.parseInt(st.nextToken());
                 map.remove(key);
-            }else if(input.equals("find")){
+            }else{
+                key =Integer.parseInt(st.nextToken());
                 if(map.containsKey(key)){
                     System.out.println(map.get(key));
                 }else{
