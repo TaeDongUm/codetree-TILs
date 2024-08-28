5b46049f19853d6439214840dcf03b617921fc91
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-08-29 00:46:54 +0900

    Title: 세 수의 최대 곱, Time: 962ms, Memory: 38MB, Status: Passed - CodeTree

--- 240829/세 수의 최대 곱/maximum-product-of-three-numbers.java
+++ 240829/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -1,26 +1,50 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int n = Integer.parseInt(br.readLine());
-        int[] num = new int[n];
-        st = new StringTokenizer(br.readLine());
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n;
+    public static int[] arr = new int[MAX_N + 1];
+    
+    public static int ans;
+    public static int negative, zero, positive;
 
-        for(int i=0;i<n;i++){
-            int tmpNum = Integer.parseInt(st.nextToken());
-            num[i] = tmpNum;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+            arr[i] = sc.nextInt();
+
+        Arrays.sort(arr, 1, n + 1);
+        for(int i = 1; i <= n; i++) {
+            if(arr[i] < 0) negative++;
+            if(arr[i] == 0) zero++;
+            if(arr[i] > 0) positive++;
         }
-        Arrays.sort(num);
-        int max1 = 0;
-        int max2 = 0;
 
-        max1 = num[n-3]*num[n-2]*num[n-1];
-        max2 = num[0]*num[1]*num[n-1];
-
-        int answer = Math.max(max1, max2);
-        System.out.println(answer);
+        // 곱 중 양수가 존재할 때
+        // 양수 3개의 곱이나, 양수 1개와 음수 2개의 곱이 만들어져야 가능합니다.
+        if(positive >= 3 || (positive >= 1 && negative >= 2)) {
+            // 양수가 3개 이상이라면, 그 중 가장 큰 3개의 수를 곱하는 것이 최선입니다.
+            if(positive >= 3)
+                ans = Math.max(ans, arr[n] * arr[n - 1] * arr[n - 2]);
+            // 음수 2개와 양수 1개를 곱할 때에는, 음수 2개는 절댓값이 가장 큰 값을, (즉, 가장 작은 두 값)
+            // 양수 1개는 가장 큰 값을 골라 곱하는 것이 최선입니다.
+            if(positive >= 1 && negative >= 2)
+                ans = Math.max(ans, arr[n] * arr[2] * arr[1]);
+        }
+        // 곱 중 0이 존재할 때
+        else if(zero >= 1)
+            ans = 0;
+        // 곱 중 음수만 존재할 때
+        // 배열에 -밖에 없거나 (negative = 1, zero = 0, positive = 2)인 경우
+        // 이 경우 가장 절댓값이 작은 값 3개를 고르는 것이 최선입니다.
+        else
+            ans = arr[n] * arr[n - 1] * arr[n - 2];
+        
+        System.out.print(ans);
     }
 }
