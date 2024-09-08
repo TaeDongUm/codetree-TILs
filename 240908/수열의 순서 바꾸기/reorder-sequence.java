a4b099e88621e2c79e8dd7697aec3373210343dc
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-09-08 23:07:04 +0900

    Title: 수열의 순서 바꾸기, Time: 81ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240908/수열의 순서 바꾸기/reorder-sequence.java
+++ 240908/수열의 순서 바꾸기/reorder-sequence.java
@@ -2,31 +2,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    static int[] num;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        int[] num = new int[n];
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
             num[i] = Integer.parseInt(st.nextToken());
         }
-
-        // 가장 긴 연속된 오름차순 구간의 길이를 찾기
-        int maxLength = 1;
-        int currentLength = 1;
-
-        for (int i = 1; i < n; i++) {
-            if (num[i] > num[i - 1]) {
-                currentLength++;
-                maxLength = Math.max(maxLength, currentLength);
-            } else {
-                currentLength = 1;
+        int answer=0;
+        while(true){
+            if(num[0]==1){
+                if(isAlign(0,n-1)){
+                    break;
+                }
+                for(int i=0;i<n;i++){
+                    if(num[i]==n){
+                        reposition(0,i);
+                        answer++;
+                    }
+                }
             }
+            if(num[0]==n){
+                reposition(0, n-1);
+                answer++;
+            }
+            if(num[0] !=1 || num[0] != n){
+                for(int i=1;i<n;i++){
+                    if(num[0]-1 == num[i]){
+                        reposition(0, i);
+                        break;
+                    }
+                }
+                answer++;
+            }        
         }
-
-        // 이동 횟수는 총 배열의 길이 - 가장 긴 오름차순 구간의 길이
-        int answer = n - maxLength;
         System.out.println(answer);
+    }
+    public static void reposition(int pre, int move){
+        int tmpNum = num[pre];
+        for(int i=pre+1;i<=move;i++){
+            num[i-1] = num[i];
+        }
+        num[move] = tmpNum;
+        return;
+    }
+    public static boolean isAlign(int start, int end){
+
+        for(int i=start;i<end;i++){
+            if(num[i+1] -num[i] !=1){
+                return false;
+            }
+        }        
+        return true;
     }
 }
