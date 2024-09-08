6931836c1aa1a335d74acdc98c683a287d500522
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-09-08 22:43:59 +0900

    Title: 수열의 순서 바꾸기, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240908/수열의 순서 바꾸기/reorder-sequence.java
+++ 240908/수열의 순서 바꾸기/reorder-sequence.java
@@ -3,60 +3,62 @@ import java.util.*;
 
 public class Main {
     static int[] num;
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
         num = new int[n];
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             num[i] = Integer.parseInt(st.nextToken());
         }
-        int answer=0;
-        while(true){
-            if(num[0]==1){
-                if(isAlign(0,n-1)){
-                    break;
-                }
-                for(int i=0;i<n;i++){
-                    if(num[i]==n){
-                        reposition(0,i);
-                        answer++;
-                    }
-                }
-            }
-            if(num[0]==n){
-                reposition(0, n-1);
-                answer++;
+
+        int answer = 0;
+        while (true) {
+            if (isAlign(0, n - 1)) {  // 배열이 정렬되었는지 확인
                 break;
             }
-            if(num[0] !=1 && num[0] != n){
-                for(int i=1;i<n;i++){
-                    if(num[i] == num[0]-1){
+
+            if (num[0] == 1) {  // 첫 번째 요소가 1일 때
+                for (int i = 0; i < n; i++) {
+                    if (num[i] == n) {  // n을 찾으면 재배치
                         reposition(0, i);
+                        answer++;
                         break;
                     }
                 }
+            } else if (num[0] == n) {  // 첫 번째 요소가 n일 때
+                reposition(0, n - 1);  // n을 맨 뒤로 보내기
                 answer++;
-            }        
+            } else {  // 그 외의 경우
+                for (int i = 1; i < n; i++) {
+                    if (num[i] == num[0] - 1) {  // 앞에 있는 숫자보다 하나 작은 숫자를 찾으면
+                        reposition(0, i);
+                        answer++;
+                        break;
+                    }
+                }
+            }
         }
-        System.out.println(answer);
+
+        System.out.println(answer);  // 정답 출력
     }
-    public static void reposition(int pre, int move){
+
+    public static void reposition(int pre, int move) {
         int tmpNum = num[pre];
-        for(int i=pre+1;i<=move;i++){
-            num[i-1] = num[i];
+        for (int i = pre + 1; i <= move; i++) {
+            num[i - 1] = num[i];
         }
         num[move] = tmpNum;
-        return;
     }
-    public static boolean isAlign(int start, int end){
 
-        for(int i=start;i<end;i++){
-            if(num[i+1] -num[i] !=1){
+    public static boolean isAlign(int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (num[i + 1] - num[i] != 1) {
                 return false;
             }
-        }        
+        }
         return true;
     }
 }
