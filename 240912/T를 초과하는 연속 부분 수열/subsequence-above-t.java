ab800b46141cea604c32e7535c1ee2c8a84ab6de
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-09-12 20:20:00 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 107ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240912/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        int[] num = new int[n];
+        int maxLength = Integer.MIN_VALUE;
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] check = new int[n];
+
+        for(int i=0;i<n-1;i++){
+            if(num[i] > t && check[i] !=1){
+                check[i] = 1;
+                int tmpLength=1;
+                int index=i;
+                while(index < n-1 && num[index] < num[index+1]){
+                    tmpLength++;
+                    index++;
+                    check[index] = 1;
+                }
+                if(maxLength < tmpLength){
+                    maxLength = tmpLength;                    
+                }
+            }
+        }
+        System.out.println(maxLength);
+
+    }
+}
