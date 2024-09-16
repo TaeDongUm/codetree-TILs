ce10c9575072c962b5ccce46a675b57f9fb05707
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-09-17 04:26:16 +0900

    Title: 되돌아오기 2, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240917/되돌아오기 2/come-back-2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] dr = {-1,0,1,0};
+    static int[] dc = {0,1,0,-1};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        int curDir = 0;
+        int startR =0;
+        int startC =0;
+        int answer=0;
+
+        for(int i=0;i<input.length();i++){
+            if(input.charAt(i)=='F'){
+                startR += dr[curDir];
+                startC += dc[curDir];
+            }
+            if(input.charAt(i)=='L'){                
+                curDir = isCurDir(curDir +1);                
+            }
+            if(input.charAt(i)=='R'){
+                curDir = isCurDir(curDir -1);
+            }
+            if(check(startR, startC)){
+                answer = i+1;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+    public static boolean check(int x, int y){
+        if(x==0 && y==0) return true;
+        return false;
+    }
+    public static int isCurDir(int curDir){
+        if(curDir <0) return 3;
+        return (curDir % 4);
+    }
+}
