b630bb085c7315dbea017a8ebbf931b5adaf84f2
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-09-17 23:49:14 +0900

    Title: 격자 위의 편안한 상태, Time: 342ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240917/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] map;
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st =new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken())-1;
+            int c = Integer.parseInt(st.nextToken())-1;
+            map[r][c] = 1;
+            int count=0;
+            for(int j=0;j<4;j++){
+                int nr = r + dr[j];
+                int nc = c + dc[j];
+                if(nr>=0 && nr<N && nc>=0 && nc <N){
+                    if(map[nr][nc]==1){
+                        count++;
+                    }
+                }
+            }
+            if(count ==3){
+                System.out.println(1);                
+            }else{
+                System.out.println(0);
+            }
+        }
+    }
+}
