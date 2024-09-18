f54daf37ea3a68d468d8356b86e26efb9795d0a1
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-09-19 00:02:53 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 107ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240919/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] dr = {0,1,0,-1};
+    static int[] dc = {1,0,-1,0};
+    static int n,m;
+    static int[][] visited;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st =new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        char[][] alpha = new char[n][m];
+        visited= new int[n][m];
+        int num=0;
+        int r = 0;
+        int c = 0;
+        int dir=0;
+        int condition=0;
+
+        while(num<n*m){
+            char ch = (char)('A'+num%26);
+            alpha[r][c] = ch;
+            visited[r][c] =1;
+            int nr = r + dr[dir];
+            int nc = c + dc[dir];
+            if(isVisited(nr,nc)){
+                r = nr;
+                c = nc;
+                num++;
+            }else{
+                dir = (dir +1) %4;
+                r = r + dr[dir];
+                c = c + dc[dir];   
+                num++;
+            }
+
+        }
+        StringBuilder sb =new StringBuilder();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                sb.append(alpha[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+    public static boolean isVisited(int r, int c){
+        if(!(r>=0 && r<n && c>=0 && c<m)) return false;
+        if(visited[r][c] !=0) return false;
+        return true;
+    }
+
+}
