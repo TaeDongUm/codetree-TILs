dc17e3367e15d6df3d72aef7a70af117be697a5c
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-09-27 20:52:06 +0900

    Title: Time to Time, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240927/Time to Time/time-to-time.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int time1 = a*60+b;
+        int time2 = c*60+d;
+        System.out.println(time2-time1);
+    }
+}
