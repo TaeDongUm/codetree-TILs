3fec378636cbd199a88c3ae64edbe7b0f16d1d68
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-09-28 00:21:18 +0900

    Title: 요일 맞추기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240928/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static String[] days ={"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    static int[] monthDays = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int daysIdx =1;
+
+        int m1 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+
+        int m2 = Integer.parseInt(st.nextToken());
+        int d2 = Integer.parseInt(st.nextToken());
+        int diff1=0;
+        int diff2=0;
+        for(int i=0;i<m1;i++){
+            diff1 += monthDays[i];
+        }
+        for(int i=0;i<m2;i++){
+            diff2 += monthDays[i];
+        }
+        diff1 = diff1 + d1;
+        diff2 = diff2 + d2;
+        int diff = diff2 - diff1;
+        boolean isMinus = false;
+        // while(true){
+        //     if(m1==m2 && d1==d2){
+        //         break;
+        //     }
+        //     d1++;
+        //     daysIdx++;
+        //     if(d1>monthDays[m1]){
+        //         d1=1;
+        //         m1++;
+        //     }
+        //     if(daysIdx>6){
+        //         daysIdx = daysIdx % 7;
+        //     }
+        // }
+        daysIdx=(7+diff%7 + daysIdx )%7;
+        System.out.println(days[daysIdx]);
+        // if(diff<0){
+        //     isMinus = true;
+        //     diff = 7- (diff*(-1) % 7);
+        //     daysIdx = (daysIdx + diff)% 7;
+        //     System.out.println(days[daysIdx]);
+        // }else{
+        //     daysIdx = (daysIdx + diff)% 7;
+        //     System.out.println(days[daysIdx]);
+        // }
+
+    }
+}
