82c4766a627cdcc19c9c3414bb5be22568fa6b7b
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-09-29 20:26:10 +0900

    Title: 2진수로 변환하기, Time: 112ms, Memory: 8MB, Status: Passed - CodeTree

--- 240929/2진수로 변환하기/convert-to-binary.java
+++ 240929/2진수로 변환하기/convert-to-binary.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
         LinkedList<Integer> digit = new LinkedList<>();
@@ -17,8 +19,11 @@ public class Main {
             n = n/2;
         }
         for(int i=digit.size()-1;i>=0;i--){
-            System.out.print(digit.get(i));
+            sb.append(digit.get(i));
         }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
         
     }
 }
