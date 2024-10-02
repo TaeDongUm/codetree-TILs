69067e3cd1874393f2ff861d29191ad82da88a5e
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-10-02 20:28:54 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 94ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241002/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 241002/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -12,8 +12,8 @@ public class Main {
         int first_y1 = Integer.parseInt(st.nextToken())+1000;
         int first_x2 = Integer.parseInt(st.nextToken())+1000;
         int first_y2 = Integer.parseInt(st.nextToken())+1000;
-        for(int j=first_y1;j<=first_y2;j++){
-            for(int i=first_x1;i<=first_x2;i++){
+        for(int j=first_y1;j<first_y2;j++){
+            for(int i=first_x1;i<first_x2;i++){
                 map[j][i] = 1;
             }
         }
@@ -23,32 +23,32 @@ public class Main {
         int sec_y1 = Integer.parseInt(st.nextToken())+1000;
         int sec_x2 = Integer.parseInt(st.nextToken())+1000;
         int sec_y2 = Integer.parseInt(st.nextToken())+1000;
-        for(int j=sec_y1;j<=sec_y2;j++){
-            for(int i=sec_x1;i<=sec_x2;i++){
+        for(int j=sec_y1;j<sec_y2;j++){
+            for(int i=sec_x1;i<sec_x2;i++){
                 map[j][i] = 2;
             }
         }
         int maxX=0;
         int maxY=0;
-        for(int j=first_y1;j<=first_y2;j++){
+        for(int j=first_y1;j<first_y2;j++){
             int tmpLengthX=0;
-            for(int i=first_x1;i<=first_x2;i++){
+            for(int i=first_x1;i<first_x2;i++){
                 if(map[j][i]==1){
                     tmpLengthX++;
                 }
             }
             maxX = Math.max(maxX, tmpLengthX);
         }
-        for(int i=first_x1;i<=first_x2;i++){
+        for(int i=first_x1;i<first_x2;i++){
             int tmpLengthY=0;
-            for(int j=first_y1;j<=first_y2;j++){
+            for(int j=first_y1;j<first_y2;j++){
                 if(map[j][i]==1){
                     tmpLengthY++;
                 }
             }
             maxY = Math.max(maxY, tmpLengthY);
         }
-        int answer = (maxX-1)*(maxY-1);
+        int answer = (maxX)*(maxY);
         System.out.println(answer);
 
     }
