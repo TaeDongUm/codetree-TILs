7c63ec159b92887423e6afc45c810e078993ef94
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-10-06 13:06:35 +0900

    Title: 정수 묶음, Time: 3015ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

--- 241006/정수 묶음/bunch-of-integers.java
+++ 241006/정수 묶음/bunch-of-integers.java
@@ -2,23 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] num;
+    static int m;
+    static int answer = Integer.MIN_VALUE;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        int[] num = new int[n];
+        num = new int[n];
+        int[] visited = new int[n];
         st = new StringTokenizer(br.readLine());
-        int numSum=0;
         for(int i=0;i<n;i++){
-            num[i] = Integer.parseInt(st.nextToken());            
-            numSum += num[i];
+            num[i] = Integer.parseInt(st.nextToken());
         }
-        int m = Integer.parseInt(br.readLine());
-        Arrays.sort(num);
-        for(int i=0;i<n-m*3;i++){
-            numSum -= num[i];
-        }
-        System.out.println(numSum);
+        m = Integer.parseInt(br.readLine());
+        group(0, 0, visited);
+        System.out.println(answer);
 
+    }
+    public static void group(int Idx, int count, int[] visited){
+        if(count ==3){
+            int numSum=0;
+            for(int i=0;i<visited.length;i++){
+                if(visited[i]==1){
+                    numSum += num[i];
+                }
+            }
+            answer = Math.max(answer, numSum);            
+        }
+        for(int i=Idx;i<visited.length-m+1;i++){
+            for(int j=i;j<i+m;j++){
+                if(visited[j]==1) continue;
+                visited[j] =1;
+            }
+            group(Idx+m,count+1,visited);
+            for(int j=i;j<i+m;j++){
+                visited[j] =0;
+            }
+            
+        }
     }
 }
