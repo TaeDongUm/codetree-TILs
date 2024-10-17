f81468d6525e811c8028be029f8b87fa73d6d722
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-10-17 15:01:20 +0900

    Title: 거품 정렬 구현, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

--- 241017/거품 정렬 구현/implement-bubble-sort.java
+++ 241017/거품 정렬 구현/implement-bubble-sort.java
@@ -13,13 +13,15 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         int count=n;
-        while(count>0){
-            count--;
+        boolean isSorted = true;
+        while(isSorted){
+            isSorted = false;            
             for(int i=0;i<arr.length-1;i++){
                 if(arr[i]>arr[i+1]){
                     int tmp = arr[i];
                     arr[i] = arr[i+1];
                     arr[i+1] = tmp;
+                    isSorted= true;
                 }
             }
         }
