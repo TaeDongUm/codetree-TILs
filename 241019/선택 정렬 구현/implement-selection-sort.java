71ead0c72e132c094d7545e2c641159c73439a98
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2024-10-19 23:23:07 +0900

    Title: 선택 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241019/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] numArr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0;i<numArr.length;i++){
+            int min=i;
+            for(int j=i;j<numArr.length;j++){
+                if(numArr[min] > numArr[j]){
+                    min = j;
+                }
+            }
+$0
+            int tmp = numArr[i];
+            numArr[i] = numArr[min];
+            numArr[min] = tmp;
+        }
+        for(int k=0;k<numArr.length;k++){
+            System.out.print(numArr[k]+" ");
+        }
+    }
+}
