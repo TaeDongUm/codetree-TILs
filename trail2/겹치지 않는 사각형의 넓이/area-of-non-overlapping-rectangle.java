110996872e0b6ee63084fb9fc6efa643f2987fbf
TaeDongUm <97272055+TaeDongUm@users.noreply.github.com>
2026-07-01 21:39:44 +0900

    [Passed] 겹치지 않는 사각형의 넓이 (193ms, 35MB)

---
+++ trail2/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+
+        int[][] coordinate = new int[2001][2001];
+        int area = 0;
+
+        for(int i=0; i<2; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int ax1 = Integer.parseInt(st.nextToken()) + 1000;
+            int ay1 = Integer.parseInt(st.nextToken()) + 1000;
+            int ax2 = Integer.parseInt(st.nextToken()) + 1000;
+            int ay2 = Integer.parseInt(st.nextToken()) + 1000;
+
+            for(int j=ax1; j<ax2;j++) {
+
+                for(int k=ay1; k<ay2; k++) {
+                    coordinate[j][k] = 1;                
+                }
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        int ax1 = Integer.parseInt(st.nextToken()) + 1000;
+        int ay1 = Integer.parseInt(st.nextToken()) + 1000;
+        int ax2 = Integer.parseInt(st.nextToken()) + 1000;
+        int ay2 = Integer.parseInt(st.nextToken()) + 1000;
+
+        for(int i=ax1; i<ax2; i++) {
+
+            for(int j=ay1;j<ay2;j++) {
+                coordinate[i][j] = 0;
+            }
+        }
+
+        for(int i=0;i<2001;i++) {
+            for(int j=0;j<2001;j++) {
+                if(coordinate[i][j]==1) {
+                    area++;
+                }
+            }
+        }
+
+        System.out.println(area);
+
+    }
+}
